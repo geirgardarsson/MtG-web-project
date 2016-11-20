b423174a8ecf31da273dcc2ec486491021e813ab
Geir Gardarsson <geg42@hi.is>
2016-11-20 20:34:03 +0000

    downloadaði mtgsdk og test fyrir danna

--- routes/cardsearch.js
+++ routes/cardsearch.js
@@ -8,13 +8,13 @@ const instance = axios.create({ baseURL });
 function start() {
   // baseURL virkar ekki þannig línar fyrir neðan er backup á meðan síðan er
   // í vinnslu
-  const instance = axios.create({ baseURL: "https://api.magicthegathering.io/v1/cards"});
+  const instance = axios.create({ baseURL: "https://api.magicthegathering.io/v1/cards?set=ktk"});
   return instance.get('/');
 }
 
 
 function cards() {
-  const instance = axios.create({ baseURL: "https://api.magicthegathering.io/v1/cards"});
+  const instance = axios.create({ baseURL: "https://api.magicthegathering.io/v1/cards?set=ktk"});
   return instance.get('/');
 }
 
