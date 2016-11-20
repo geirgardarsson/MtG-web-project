9c6c8e691e7491d52c73058ab1af30fb33e34526
Geir Gardarsson <geg42@hi.is>
2016-11-20 18:32:40 +0000

    tók til í routes, og cardsearch.js

--- routes/cardsearch.js
+++ routes/cardsearch.js
@@ -1,10 +1,8 @@
 const axios = require('axios');
 
-const baseURL = ''; // Sækja úr environment breytu
-const instance = axios.create({ baseURL })
-
-
-
+const baseURL = process.env.BASEURL ||
+                'https://api.magicthegathering.io/v1/cards';
+const instance = axios.create({ baseURL });
 
 function cards() {
   const instance = axios.create({baseURL: "https://api.magicthegathering.io/v1/cards"});
--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,29 @@ const cardsearch = require('./cardsearch.js');
 const axios = require('axios');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
+  res.render('index', {title: 'Magic the Gathering'})
+  .catch((error) => {
+    res.render('error', { title: 'Villa kom upp', message: 'U n00b'});
+  });
+});
+
+
+
+
+
+
+router.get('/card', (req, res, next) => {
   cardsearch.cards()
   .then((result) => {
     console.log(result.data.cards);
     res.render('card', { title: 'Magic the Gathering', card: result.data.cards });
   })
   .catch((error) => {
-    res.render('error', { title: 'Villa kom upp', error});
+    res.render('error', { title: 'Villa kom upp', message: 'U don goof'});
   });
 });
+
+// annað router.get function hér
 
 module.exports = router;
